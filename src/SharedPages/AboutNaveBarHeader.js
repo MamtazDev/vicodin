63229df9612f1a6e38cdb22ac36d9c5e9946b048
zerin22 <tajrin.ini@gmail.com>
2023-10-30 16:13:17 +0600

    About Navbar Edit Mobile version

--- src/SharedPages/AboutNaveBarHeader.js
+++ src/SharedPages/AboutNaveBarHeader.js
@@ -16,10 +16,10 @@ const AboutNaveBarHeader = () => {
                                 <div className="ltn__top-bar-menu">
                                     <ul>
                                         <li>
-                                            <a href="mailto:info@webmail.com?Subject=Flower%20greetings%20to%20you">
-                                                <i className="fas fa-envelope"></i> info@webmail.com</a>
+                                            <NavLink to="mailto:info@webmail.com?Subject=Flower%20greetings%20to%20you">
+                                                <i className="fas fa-envelope"></i> info@webmail.com</NavLink>
                                         </li>
-                                        <li><a href="locations.html"><i className="fa-solid fa-location-dot"></i> 15/A, Nest Tower, NYC</a></li>
+                                        <li><NavLink to="/contact"><i className="fa-solid fa-location-dot"></i> 15/A, Nest Tower, NYC</NavLink></li>
                                     </ul>
                                 </div>
                             </div>
@@ -121,23 +121,15 @@ const AboutNaveBarHeader = () => {
                                             </div>
                                         </li>
                                         <li>
-                                            {/* <!-- mini-cart 2 --> */}
-                                            <div className="mini-cart-icon" type="button" >
-                                                <NavLink to="cart" className="ltn__utilize-toggle">
-                                                    <i className="fa-solid fa-cart-shopping"></i>
-                                                    <sup>2</sup>
-                                                </NavLink>
-                                            </div>
-
-                                            {/* <div className="mini-cart-icon mini-cart-icon-2">
-                                                <a href="#ltn__utilize-cart-menu" className="ltn__utilize-toggle" >
+                                            <div className="mini-cart-icon mini-cart-icon-2">
+                                                <NavLink to="/cart" className="ltn__utilize-toggle" >
                                                     <span className="mini-cart-icon">
                                                         <i className="fa-solid fa-cart-shopping"></i>
                                                         <sup>2</sup>
                                                     </span>
                                                     <h6><span>Your Cart</span> <span className="ltn__secondary-color">$89.25</span></h6>
-                                                </a>
-                                            </div> */}
+                                                </NavLink>
+                                            </div>
                                         </li>
                                     </ul>
                                 </div>
@@ -168,6 +160,7 @@ const AboutNaveBarHeader = () => {
                                                 <li><NavLink to="/service">Service</NavLink></li>
                                                 <li><NavLink to="/shopgrid">Shop</NavLink></li>
                                                 <li><NavLink to="/contact">Contact</NavLink></li>
+
                                             </ul>
                                         </div>
                                     </nav>
@@ -191,13 +184,13 @@ const AboutNaveBarHeader = () => {
                             {/* <!-- Mobile Menu Button --> */}
                             <div className="mobile-menu-toggle d-lg-none">
                                 <span>MENU</span>
-                                <div href="" className="ltn__utilize-toggle">
+                                <NavLink href="" className="ltn__utilize-toggle" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWith" aria-controls="offcanvasWithBothOptions">
                                     <svg viewBox="0 0 800 600">
                                         <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
                                         <path d="M300,320 L540,320" id="middle"></path>
                                         <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
                                     </svg>
-                                </div>
+                                </NavLink>
                             </div>
                         </div>
                     </div>
@@ -205,88 +198,72 @@ const AboutNaveBarHeader = () => {
             </div>
             {/* <!-- MOBILE MENU END --> */}
 
-         
+
 
             {/* <!-- Utilize Mobile Menu Start --> */}
-            <div id="ltn__utilize-mobile-menu" className="ltn__utilize ltn__utilize-mobile-menu">
-                <div className="ltn__utilize-menu-inner ltn__scrollbar">
-                    <div className="ltn__utilize-menu-head">
-                        <div className="site-logo">
-                            <NavLink to="/"><img src={Logo} alt="Logo" /></NavLink>
+            <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWith" aria-labelledby="offcanvasWithBothOptionsLabel">
+                <div class="offcanvas-body">
+                    <div className="ltn__utilize-mobile-menu">
+                        <div className="ltn__utilize-menu-inner ltn__scrollbar">
+                            <div className="ltn__utilize-menu-head">
+                                <div className="site-logo">
+                                    <NavLink to="/"><img src={Logo} alt="Logo" /></NavLink>
+                                </div>
+                                <button className="ltn__utilize-close">×</button>
+                            </div>
+                            <div className="ltn__utilize-menu-search-form">
+                                <form action="#">
+                                    <input type="text" placeholder="Search..." />
+                                    <button><i className="fa-solid fa-magnifying-glass for-search-show"></i></button>
+                                </form>
+                            </div>
+                            <div className="ltn__utilize-menu">
+                                <ul>
+                                    <li><NavLink to="/">Home</NavLink></li>
+                                    <li><NavLink to="/about">About</NavLink></li>
+                                    <li><NavLink to="/service">Service</NavLink></li>
+                                    <li ><NavLink to="/shopgrid">Shop Grid</NavLink> </li>
+                                    <li><NavLink to="/contact">Contact</NavLink></li>
+                                </ul>
+                            </div>
+                            <div className="ltn__utilize-buttons ltn__utilize-buttons-2">
+                                <ul>
+                                    <li><NavLink to="/account" title="My Account">
+                                        <span className="utilize-btn-icon">
+                                            <i className="far fa-user"></i>
+                                        </span>
+                                        My Account
+                                    </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to="/wishlist" title="Wishlist">
+                                            <span className="utilize-btn-icon">
+                                                <i className="far fa-heart"></i>
+                                                <sup>3</sup>
+                                            </span>
+                                            Wishlist
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to="/cart" title="Shoping Cart">
+                                            <span className="utilize-btn-icon">
+                                                <i className="fas fa-shopping-cart"></i>
+                                                <sup>5</sup>
+                                            </span>
+                                            Shoping Cart
+                                        </NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="ltn__social-media-2">
+                                <ul>
+                                    <li><NavLink to="#" title="Facebook"><i className="fab fa-facebook-f"></i></NavLink></li>
+                                    <li><NavLink to="#" title="Twitter"><i className="fab fa-twitter"></i></NavLink></li>
+                                    <li><NavLink to="#" title="Linkedin"><i className="fab fa-linkedin"></i></NavLink></li>
+                                    <li><NavLink to="#" title="Instagram"><i className="fab fa-instagram"></i></NavLink></li>
+                                </ul>
+                            </div>
                         </div>
-                        <button className="ltn__utilize-close">×</button>
-                    </div>
-                    <div className="ltn__utilize-menu-search-form">
-                        <form action="#">
-                            <input type="text" placeholder="Search..." />
-                            <button><i className="fa-solid fa-magnifying-glass for-search-show"></i></button>
-                        </form>
-                    </div>
-                    <div className="ltn__utilize-menu">
-                        <ul>
-                            <li><NavLink to="/">Home</NavLink>
-                            </li>
-                            <li><a href="#">About</a>
-                                <ul className="sub-menu">
-                                    <li><NavLink to="/about">About</NavLink></li>
-                                    <li><NavLink to="/service">Services</NavLink></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Shop</a>
-                                <ul className="sub-menu">
-                                    <li><a href="shop.html">Shop</a></li>
-                                    <li><NavLink to="/shopgrid">Shop Grid</NavLink></li>
-                                    <li><a href="account.html">My Account</a></li>
-                                    <li><NavLink to="/login">Sign in</NavLink></li>
-                                    <li><NavLink to="/register">Register</NavLink></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Pages</a>
-                                <ul className="sub-menu">
-                                    <li><NavLink to="/about">About</NavLink></li>
-                                    <li><NavLink to="/service">Services</NavLink></li>
-                                </ul>
-                            </li>
-                            <li><NavLink to="/contact">Contact</NavLink></li>
-                        </ul>
-                    </div>
-                    <div className="ltn__utilize-buttons ltn__utilize-buttons-2">
-                        <ul>
-                            <li>
-                                <a href="account.html" title="My Account">
-                                    <span className="utilize-btn-icon">
-                                        <i className="far fa-user"></i>
-                                    </span>
-                                    My Account
-                                </a>
-                            </li>
-                            <li>
-                                <a href="wishlist.html" title="Wishlist">
-                                    <span className="utilize-btn-icon">
-                                        <i className="far fa-heart"></i>
-                                        <sup>3</sup>
-                                    </span>
-                                    Wishlist
-                                </a>
-                            </li>
-                            <li>
-                                <a href="cart.html" title="Shoping Cart">
-                                    <span className="utilize-btn-icon">
-                                        <i className="fas fa-shopping-cart"></i>
-                                        <sup>5</sup>
-                                    </span>
-                                    Shoping Cart
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="ltn__social-media-2">
-                        <ul>
-                            <li><a href="#" title="Facebook"><i className="fab fa-facebook-f"></i></a></li>
-                            <li><a href="#" title="Twitter"><i className="fab fa-twitter"></i></a></li>
-                            <li><a href="#" title="Linkedin"><i className="fab fa-linkedin"></i></a></li>
-                            <li><a href="#" title="Instagram"><i className="fab fa-instagram"></i></a></li>
-                        </ul>
                     </div>
                 </div>
             </div>
--- src/SharedPages/Header.js
+++ src/SharedPages/Header.js
@@ -80,7 +80,7 @@ const header = () => {
                                                     <li><NavLink to="/about">About</NavLink></li>
                                                     <li><NavLink to="/service">Service</NavLink>
                                                     </li>
-                                                    <li ><NavLink to="/shopgrid">Shop Grid</NavLink>
+                                                    <li ><NavLink to="/shopgrid">Shop</NavLink>
                                                     </li>
                                                     <li><NavLink to="/contact">Contact</NavLink></li>
                                                 </ul>
@@ -134,15 +134,6 @@ const header = () => {
 
                                     <!-- Mobile Menu Button --> */}
 
-                                    {/* <div className="mobile-menu-toggle d-xl-none" type="button" >
-                                        <div className="ltn__utilize-toggle">
-                                            <svg viewBox="0 0 800 600">
-                                                <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
-                                                <path d="M300,320 L540,320" id="middle"></path>
-                                                <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
-                                            </svg>
-                                        </div>
-                                    </div> */}
                                     <div className="mobile-menu-toggle d-xl-none " >
                                         <NavLink type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
                                             <svg viewBox="0 0 800 600">
@@ -172,12 +163,9 @@ const header = () => {
                                                     <div className="ltn__utilize-menu">
                                                         <ul>
                                                             <li><NavLink to="/">Home</NavLink></li>
-
                                                             <li><NavLink to="/about">About</NavLink></li>
-                                                            <li><NavLink to="/service">Service</NavLink>
-                                                            </li>
-                                                            <li ><NavLink to="/shopgrid">Shop Grid</NavLink>
-                                                            </li>
+                                                            <li><NavLink to="/service">Service</NavLink></li>
+                                                            <li ><NavLink to="/shopgrid">Shop Grid</NavLink> </li>
                                                             <li><NavLink to="/contact">Contact</NavLink></li>
                                                         </ul>
                                                     </div>
@@ -212,10 +200,10 @@ const header = () => {
                                                     </div>
                                                     <div className="ltn__social-media-2">
                                                         <ul>
-                                                            <li><a href="#" title="Facebook"><i className="fab fa-facebook-f"></i></a></li>
-                                                            <li><a href="#" title="Twitter"><i className="fab fa-twitter"></i></a></li>
-                                                            <li><a href="#" title="Linkedin"><i className="fab fa-linkedin"></i></a></li>
-                                                            <li><a href="#" title="Instagram"><i className="fab fa-instagram"></i></a></li>
+                                                            <li><NavLink to="#" title="Facebook"><i className="fab fa-facebook-f"></i></NavLink></li>
+                                                            <li><NavLink to="#" title="Twitter"><i className="fab fa-twitter"></i></NavLink></li>
+                                                            <li><NavLink to="#" title="Linkedin"><i className="fab fa-linkedin"></i></NavLink></li>
+                                                            <li><NavLink to="#" title="Instagram"><i className="fab fa-instagram"></i></NavLink></li>
                                                         </ul>
                                                     </div>
                                                 </div>
